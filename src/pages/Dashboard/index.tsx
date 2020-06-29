1ea7557a61f7cb60b391e81f73880ad899261e72
Gabriel Prando <gprando55@gmail.com>
2020-06-29 11:09:18 -0300

    styled dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,70 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="/">
+        <img
+          src="https://avatars0.githubusercontent.com/u/50037292?s=460&u=c9143ea90d7f81261eba361280672ea210469aa4&v=4"
+          alt="Gabriel Prando"
+        />
+
+        <div>
+          <strong>gprando/guia_git</strong>
+          <p>Um guia rápido e funcional de git</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="/">
+        <img
+          src="https://avatars0.githubusercontent.com/u/50037292?s=460&u=c9143ea90d7f81261eba361280672ea210469aa4&v=4"
+          alt="Gabriel Prando"
+        />
+
+        <div>
+          <strong>gprando/guia_git</strong>
+          <p>Um guia rápido e funcional de git</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="/">
+        <img
+          src="https://avatars0.githubusercontent.com/u/50037292?s=460&u=c9143ea90d7f81261eba361280672ea210469aa4&v=4"
+          alt="Gabriel Prando"
+        />
+
+        <div>
+          <strong>gprando/guia_git</strong>
+          <p>Um guia rápido e funcional de git</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="/">
+        <img
+          src="https://avatars0.githubusercontent.com/u/50037292?s=460&u=c9143ea90d7f81261eba361280672ea210469aa4&v=4"
+          alt="Gabriel Prando"
+        />
+
+        <div>
+          <strong>gprando/guia_git</strong>
+          <p>Um guia rápido e funcional de git</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
